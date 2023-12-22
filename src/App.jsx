218312002507874044857166f1ff3705d051266a
Kaushik Nitin Naik <kaushik.naik1602@gmail.com>
2023-12-22 18:31:19 +0530

    Route Change

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,10 @@
-import {
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
-} from "react-router-dom";
 import "./App.css";
 
 import Login from "./components/Login";
-import Dashboard from "./components/Dashboard";
-import Welcome from "./components/Welcome";
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Login />}>
-      <Route index element={<Login />} />
-      <Route path="dashboard" element={<Dashboard />} />
-      <Route path="welcome" element={<Welcome />} />
-    </Route>
-  )
-);
-
 function App() {
   return (
     <>
-      <RouterProvider router={router} />
+      <Login></Login>
     </>
   );
 }
